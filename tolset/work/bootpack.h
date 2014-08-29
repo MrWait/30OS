f86a75a8221b144e5fbf153012cd98c9cfcf9596
MrWait <hanlianzhen@gmail.com>
2014-08-29 22:55:53 +0800

    haribote04e

--- tolset/work/bootpack.h
+++ tolset/work/bootpack.h
@@ -23,6 +23,16 @@ void asm_inthandler21(void);
 void asm_inthandler27(void);
 void asm_inthandler2c(void);
 
+struct FIFO8
+{
+    unsigned char *buf;
+    int p, q, size, free, flags;
+};
+void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf);
+int fifo8_put(struct FIFO8 *fifo, unsigned char data);
+int fifo8_get(struct FIFO8 *fifo);
+int fifo8_status(struct FIFO8 *fifo);
+
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
 void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
@@ -79,11 +89,6 @@ void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
 #define AR_INTGATE32 0x008e
 
 
-struct KEYBUF
-{
-  unsigned char data[32];
-  int next_r, next_w, len;
-};
 void init_pic(void);
 void inthandler21(int *esp);
 void inthandler27(int *esp);
