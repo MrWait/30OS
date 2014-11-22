3a58a2c83add8dc215ee5323a263d4badf36de43
master <hanlianzhen@gmali.com>
2014-11-22 20:17:32 +0800

    haribote15f

--- tolset/work/bootpack.c
+++ tolset/work/bootpack.c
@@ -1,5 +1,6 @@
 #include "bootpack.h"
 #include <stdio.h>
+#include <string.h>
 
 void make_window8(unsigned char *buf, int xsize, int ysize, char *title, char act);
 void putfonts8_asc_sht(struct SHEET *sht, int x, int y, int c, int b, char *s, int l);
@@ -435,7 +436,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
     int i, fifobuf[128], cursor_x = 16, cursor_y = 28, cursor_c = -1;
     char s[30], cmdline[30];
     struct MEMMAN *memman = (struct MEMMAN*)MEMMAN_ADDR;
-
+    int x, y;
     fifo32_init(&task->fifo, 128, fifobuf, task);
     timer = timer_alloc();
     timer_init(timer, &task->fifo, 1);
@@ -497,7 +498,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                     putfonts8_asc_sht(sheet, cursor_x, cursor_y, COL8_FFFFFF, COL8_000000, " ", 1);
                     cmdline[cursor_x / 8 - 2] = 0;
                     cursor_y = cons_newline(cursor_y, sheet);
-                    if(cmdline[0] == 'm' && cmdline[1] == 'e' && cmdline[2] == 'm' && cmdline[3] == 0)
+                    if(strcmp(cmdline, "mem") == 0)
                     {
                         sprintf(s, "total %dMB", memtotal / (1024 * 1024));
                         putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
@@ -506,6 +507,18 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                         putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, s, 30);
                         cursor_y = cons_newline(cursor_y, sheet);
                         cursor_y = cons_newline(cursor_y, sheet);
+                    }
+                    else if(strcmp(cmdline, "cls") == 0)
+                    {
+                        for(y = 28; y < 28 + 128; y++)
+                        {
+                            for(x = 8; x < 8 + 240; x++)
+                            {
+                                sheet->buf[x + y * sheet->bxsize] = COL8_000000;
+                            }
+                        }
+                        sheet_refresh(sheet, 8, 28, 8 + 240, 28 + 128);
+                        cursor_y = 28;
                     }
                     else if(cmdline[0] != 0)
                     {
